92f40926075931e17c0721c97d97bf0845409141
Jérémie Grodziski <jeremie@grodziski.com>
2023-03-11 10:36:40 +0100

    Fix RSS

--- src/defsquare/rss.clj
+++ src/defsquare/rss.clj
@@ -3,22 +3,23 @@
             [defsquare.file-utils :refer [file-separator]]))
 
 (defn blog-post-link [{:keys [baseurl from] :as params} markdown]
+  ;(println "blog-post-link" (:metadata markdown))
   (str baseurl "/" from "/" (get-in markdown [:path :name])))
 
 (defn export-rss! [params markdowns]
   ;(println "export-rss!" params markdowns)
   (let [rss-xml (rss/channel-xml
-                 {:title       (:title params)
-                  :link        (str (:baseurl params) "/" (:from params))
-                  :feed-url    (str (:baseurl params) "/" (:from params) "/rss.xml")
-                  :description (:description params)}
-                 (filter identity (map (fn [markdown]
-                                         (let [{:keys [timestamp title author path summary] :as metadata} (:metadata markdown)]
-                                           ;(println "export-rss! item" title author summary)
-                                           (when metadata
-                                             {:title       title
-                                              :author      author
-                                              :description summary
-                                              :link        (blog-post-link params markdown)}))) markdowns)))]
+                       {:title       (:title params)
+                        :link        (str (:baseurl params) "/" (:from params))
+                        :feed-url    (str (:baseurl params) "/" (:from params) "/rss.xml")
+                        :description (:description params)}
+                       (filter identity (map (fn [markdown]
+                                               (let [{:keys [timestamp title author path summary] :as metadata} (:metadata markdown)]
+                                        ;(println "export-rss! item" title author summary)
+                                                 (when metadata
+                                                   {:title       title
+                                                    :author      author
+                                                    :description summary
+                                                    :link        (blog-post-link params markdown)}))) markdowns)))]
     (println (format "Export RSS feed to rss.xml"))
     (spit (str (:to params) (file-separator) "rss.xml") rss-xml)))
